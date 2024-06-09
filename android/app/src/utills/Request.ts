ff73e669f01a6b2e9d1ad86887f724488fffd365
Lseoksee <da864268@naver.com>
2024-06-09 10:06:52 +0900

    요청 코드 완성

--- android/app/src/utills/Request.ts
+++ android/app/src/utills/Request.ts
@@ -1,3 +1,65 @@
+/**  데이터 백엔드에 get 요청을 보냅니다.
+ * @param url 요청주소
+ * @returns 응답 json
+ * @example
+ * // async, await 방식
+ * async function any() {
+ *   const data = await reqGet('url');
+ *   console.log(data.아무개);
+ * }
+ * // callback 방식
+ * reqGet('url')
+ *  .then(data => console.log(data.아무개))
+ *  .catch(err => console.log(err));
+ */
+export async function reqGet(url: string): Promise<any> {
+  // url 에 /있는지 판단
+  if (url.slice(-1) !== '/') {
+    url = url + '/';
+  }
+
+  const res = await fetch(url, {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+
+  return await res.json();
+}
+
+/** 데이터 백엔드에 post 요청을 보냅니다.
+ * @param url 요청주소
+ * @param body 요청 js객체
+ * @returns 응답 json
+ * @example
+ * // async, await 방식
+ * async function any() {
+ *   const data = await reqGet('url', {키: '값'});
+ *   console.log(data.아무개);
+ * }
+ * // callback 방식
+ * reqGet('url', {키: '값'})
+ *  .then(data => console.log(data.아무개))
+ *  .catch(err => console.log(err));
+ */
+export async function reqPost(url: string, body: object): Promise<any> {
+  // url 에 /있는지 판단
+  if (url.slice(-1) !== '/') {
+    url = url + '/';
+  }
+
+  const res = await fetch(url, {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+
+  return await res.json();
+}
+
 /** AR백엔드에 요청을 보냅니다
  * @param url 요청주소
  * @param formData  FormData (하단 예제 참고 바람)
