55d2e73d0eae4873286a46babe444c19e4a8b1bb
Lseoksee <da864268@naver.com>
2024-06-15 17:10:38 +0900

    DATA_URL 업데이트

--- android/app/src/Constant.ts
+++ android/app/src/Constant.ts
@@ -2,4 +2,4 @@
 export const AR_URL = 'http://dmumars.kro.kr:8080/';
 
 /** 데이터 백엔드 요청 주소 */
-export const DATA_URL = 'http://korseok.kro.kr/';
+export const DATA_URL = 'http://fitpitback.kro.kr:8080/';
