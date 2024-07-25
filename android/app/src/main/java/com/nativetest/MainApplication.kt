c092a93a7531321a2d3218683d874dec80b0d79c
juzuhee <txtloverrr@naver.com>
2024-07-25 17:05:21 +0900

    구글
    구글

--- android/app/src/main/java/com/nativetest/MainApplication.kt
+++ android/app/src/main/java/com/nativetest/MainApplication.kt
@@ -11,35 +11,43 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-            }
+    override val reactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> =
+                PackageList(this).packages
 
-        override fun getJSMainModuleName(): String = "index"
+            override fun getJSMainModuleName(): String = "index"
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        }
 
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
 
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            load()
+        }
+        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+
+        // Google Sign-In 설정
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)) // 여기서 R.string.default_web_client_id를 사용합니다.
+            .requestEmail()
+            .build()
+
+        // GoogleSignInClient 인스턴스 생성 (선택적: 이 인스턴스는 다른 부분에서 사용될 수 있습니다)
+        val googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
     }
-    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
-  }
 }
--- android/app/src/screens/Join/LognSignin.tsx
+++ android/app/src/screens/Join/LognSignin.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   View,
   Text,
@@ -6,8 +6,14 @@ import {
   TouchableOpacity,
   Image,
   Dimensions,
+  Alert,
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 import type {StackNavigationProp} from '@react-navigation/stack';
 import type {RootStackParamList} from '../../../../../App';
 
@@ -16,12 +22,73 @@ const {width, height} = Dimensions.get('window');
 const LognSignin = () => {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
 
+  const googleSigninConfigure = () => {
+    GoogleSignin.configure({
+      webClientId:
+        '934828416095-s1b8nvithrntut57d35gnldpurudlfqk.apps.googleusercontent.com',
+    });
+  };
+
+  useEffect(() => {
+    googleSigninConfigure();
+  }, []);
+
   const handleSigninPress = () => {
     navigation.navigate('Signin');
   };
 
   const handleLoginPress = () => {
     navigation.navigate('Login');
+  };
+
+  const handleGoogleSignin = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const {idToken} = await GoogleSignin.signIn();
+
+      // Google credential을 Firebase에 생성
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+      // Firebase에 인증
+      await auth().signInWithCredential(googleCredential);
+
+      const currentUser = auth().currentUser;
+
+      Alert.alert('로그인 성공', `환영합니다, ${currentUser?.displayName}!`);
+      navigation.reset({
+        index: 0,
+        routes: [{name: 'BasicInformation'}],
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log('Google Sign-In Error:', error.message);
+        switch (error.message) {
+          case statusCodes.SIGN_IN_CANCELLED:
+            Alert.alert(
+              '로그인 취소됨',
+              '사용자가 로그인 과정을 취소했습니다.',
+            );
+            break;
+          case statusCodes.IN_PROGRESS:
+            Alert.alert('로그인 진행 중', '로그인 과정이 진행 중입니다.');
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            Alert.alert(
+              'Play 서비스 사용 불가',
+              'Google Play 서비스가 사용 불가능합니다.',
+            );
+            break;
+          case statusCodes.SIGN_IN_REQUIRED:
+            Alert.alert('로그인 필요', '로그인이 필요합니다.');
+            break;
+          default:
+            Alert.alert('알 수 없는 오류', '알 수 없는 오류가 발생했습니다.');
+            break;
+        }
+      } else {
+        Alert.alert('알 수 없는 오류', '알 수 없는 오류가 발생했습니다.');
+      }
+    }
   };
 
   return (
@@ -52,7 +119,9 @@ const LognSignin = () => {
         </View>
       </TouchableOpacity>
 
-      <TouchableOpacity style={[styles.button, styles.buttonOutlined]}>
+      <TouchableOpacity
+        style={[styles.button, styles.buttonOutlined]}
+        onPress={handleGoogleSignin}>
         <View style={styles.buttonContent}>
           <Image
             source={require('../../assets/img/join/google.png')}
@@ -72,21 +141,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#ffffff',
   },
-
   title: {
     fontSize: width * 0.1,
     color: '#000000',
     marginBottom: height * 0.08,
     fontWeight: 'bold',
   },
-
   or: {
     fontSize: width * 0.05,
     color: '#979797',
     marginVertical: height * 0.02,
     fontWeight: 'bold',
   },
-
   button: {
     paddingVertical: height * 0.022,
     paddingHorizontal: width * 0.1,
@@ -95,41 +161,34 @@ const styles = StyleSheet.create({
     width: '80%',
     alignItems: 'center',
   },
-
   buttonOutlined: {
     backgroundColor: '#ffffff',
     borderWidth: 1,
     borderColor: '#000000',
   },
-
   buttonFilled: {
     backgroundColor: '#000000',
   },
-
   buttonKakao: {
     backgroundColor: '#FBE300',
   },
-
   buttonText: {
     color: '#000000',
     fontSize: width * 0.04,
     textAlign: 'center',
     fontWeight: 'bold',
   },
-
   buttonText2: {
     color: '#ffffff',
     fontSize: width * 0.04,
     textAlign: 'center',
     fontWeight: 'bold',
   },
-
   buttonIcon: {
     width: width * 0.06,
     height: width * 0.06,
     marginRight: width * 0.02,
   },
-
   buttonContent: {
     flexDirection: 'row',
     alignItems: 'center',
