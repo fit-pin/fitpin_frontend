187bd7ca4b4ad3412f200300c039f34d3aa1efe1
Lseoksee <da864268@naver.com>
2024-08-09 20:52:20 +0900

    소셜로그인 구조 최적화

--- android/app/src/main/java/com/nativetest/MainApplication.kt
+++ android/app/src/main/java/com/nativetest/MainApplication.kt
@@ -11,47 +11,40 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.kakao.sdk.common.KakaoSdk // 카카오 SDK 추가
 import com.kakao.sdk.common.KakaoSdk.init // 카카오 SDK 초기화
 
 class MainApplication : Application(), ReactApplication {
 
-    override val reactNativeHost: ReactNativeHost =
-        object : DefaultReactNativeHost(this) {
-            override fun getPackages(): List<ReactPackage> =
-                PackageList(this).packages
+  override val reactNativeHost: ReactNativeHost =
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // add(MyReactNativePackage())
+            }
 
-            override fun getJSMainModuleName(): String = "index"
+        override fun getJSMainModuleName(): String = "index"
 
-            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-        }
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+      }
 
-    override val reactHost: ReactHost
-        get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
 
-    override fun onCreate() {
-        super.onCreate()
-        SoLoader.init(this, false)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load()
-        }
-        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
-
-        // Google Sign-In 설정
-        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id)) // 여기에 실제 웹 클라이언트 ID를 설정합니다.
-            .requestEmail()
-            .build()
-
-        val googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
-
-        // 카카오 SDK 초기화
-        KakaoSdk.init(this, getString(R.string.kakao_app_key)) // 여기에 실제 카카오 앱 키를 설정합니다.
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, false)
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      load()
     }
+    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+
+    // 카카오 SDK 초기화
+    KakaoSdk.init(this, getString(R.string.kakao_app_key))
+  }
 }
