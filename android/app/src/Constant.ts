c09176f2024b82f48299378e0d7deb41d16428a4
Lseoksee <da864268@naver.com>
2024-11-17 17:51:07 +0900

    서버 주소 및 readme 수정

--- android/app/src/Constant.ts
+++ android/app/src/Constant.ts
@@ -1,8 +1,8 @@
 /** AR 백엔드 요청 주소 */
-export const AR_URL = 'https://dmumars.kro.kr/';
+export const AR_URL = 'https://fitpin.kro.kr/ar-api/';
 
 /** 데이터 백엔드 요청 주소 */
-export const DATA_URL = 'http://fitpitback.kro.kr:8080/';
+export const DATA_URL = 'https://fitpin.kro.kr/app-api/';
 
 /** 수선 경매 알림 서버 주소 */
-export const AUCTION_URL = 'https://fitpin-web-back.kro.kr/';
+export const AUCTION_URL = 'https://fitpin.kro.kr/web-api';
